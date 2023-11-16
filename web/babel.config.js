be2b36e9e31e2081ad8e4c0c3532c5feef0f6caa
chengsi <chengsi.zhang@se.com>
2023-11-16 14:42:59 +0800

    react router + cssInJs

---
+++ web/babel.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  assumptions: {
+    arrayLikeIsIterable: false,
+    constantReexports: true,
+    constantSuper: true,
+    enumerableModuleMeta: true,
+    ignoreFunctionLength: true,
+    ignoreToPrimitiveHint: true,
+    iterableIsArray: true,
+    mutableTemplateObject: true,
+    noClassCalls: true,
+    noDocumentAll: true,
+    noNewArrows: true,
+    objectRestNoSymbols: true,
+    privateFieldsAsProperties: true,
+    pureGetters: true,
+    setClassMethods: true,
+    setComputedProperties: true,
+    setPublicClassFields: true,
+    setSpreadProperties: true,
+    skipForOfIteratorClosing: true,
+    superIsCallableConstructor: true,
+  },
+  presets: [
+    [
+      '@babel/env',
+      {
+        bugfixes: true,
+      },
+    ],
+    ['@babel/react'],
+    '@linaria',
+  ]
+};
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { css } from '@linaria/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactffff
-        </a>
-      </header>
+    <div className={css`
+      width: 500px;
+      height: 500px;
+      background-color: green;
+
+    `}>
+      
     </div>
   );
 }

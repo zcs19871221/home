a8f0b04d2c79474c48bed408a86d36d8f4275f7b
chengsi <chengsi.zhang@se.com>
2023-11-16 11:05:55 +0800

    ffff

---
+++ src/main/java/com/cs/home/WebController.java
@@ -0,0 +1,12 @@
+package com.cs.home;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebController {
+
+//    @RequestMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+}

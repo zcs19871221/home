2c572c67c296a449a4c9333e6811c198bc9de929
chengsi <chengsi.zhang@se.com>
2023-12-08 11:09:30 +0800

    front

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import { css } from '@linaria/core';
+
+function App() {
+  return (
+    <div
+      className={css`
+        width: 100px;
+        height: 100px;
+        background-color: aliceblue;
+        color: red;
+      `}
+    ></div>
+  );
+}
+
+export default App;
